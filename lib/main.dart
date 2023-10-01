55313e2c47ee975535df6adeb13f0eae8f792a1c
Taofik <taofik.dev@gmail.com>
2023-10-01 14:00:39 +0100

    step-5

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -35,21 +36,19 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Countdown extends ValueNotifier<int> {
-  late StreamSubscription sub;
-  Countdown({required int from}) : super(from) {
-    sub = Stream.periodic(const Duration(seconds: 1), (v) => from - v)
-        .takeWhile((element) => element >= 0)
-        .listen((value) {
-      this.value = value;
-    });
-  }
+const url = 'https://bit.ly/46oBQFe';
+const imageHeight = 300.0;
 
-  @override
-  void dispose() {
-    sub.cancel();
-    super.dispose();
-  }
+extension Normalize on num {
+  num normalized(
+    num selfRangMin,
+    num selfRangeMax, [
+    num normalizedRangeMin = 0.0,
+    num normalizedRangeMax = 1.0,
+  ]) =>
+      (normalizedRangeMax - normalizedRangeMin) *
+          ((this - selfRangMin) / (selfRangeMax - selfRangMin)) +
+      normalizedRangeMin;
 }
 
 class MyHomePage extends HookWidget {
@@ -57,15 +56,63 @@ class MyHomePage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final countDown = useMemoized(() => Countdown(from: 20));
-    final notifier = useListenable(countDown);
+    final opacity = useAnimationController(
+      duration: const Duration(seconds: 1),
+      initialValue: 1.0,
+      lowerBound: 0.0,
+      upperBound: 1.0,
+    );
+    final size = useAnimationController(
+      duration: const Duration(seconds: 1),
+      initialValue: 1.0,
+      lowerBound: 0.0,
+      upperBound: 1.0,
+    );
+
+    final controller = useScrollController();
+    useEffect(() {
+      controller.addListener(() {
+        final newOpacity = max(imageHeight - controller.offset, 0.0);
+        final normalized = newOpacity.normalized(0.0, imageHeight).toDouble();
+        opacity.value = normalized;
+        size.value = normalized;
+      });
+      return null;
+    }, [controller]);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // title: Text(dateTime.data ?? 'Home page'),
         title: const Text('Home page'),
       ),
-      body: Text(notifier.value.toString()),
+      body: Column(
+        children: [
+          SizeTransition(
+            sizeFactor: size,
+            axis: Axis.vertical,
+            axisAlignment: -1.0,
+            child: FadeTransition(
+              opacity: opacity,
+              child: Image.network(
+                url,
+                height: imageHeight,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              controller: controller,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Person ${index + 1}'),
+                );
+              },
+              itemCount: 100,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
