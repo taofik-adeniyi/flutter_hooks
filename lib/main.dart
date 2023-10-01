f9d3f5237ff9ba65b7f05d26ac4c92248bab4dd5
Taofik <taofik.dev@gmail.com>
2023-10-01 12:23:05 +0100

    use memoied and usefuture

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(
+        transform ?? (e) => e,
+      ).where((e) => e != null).cast();
+}
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -22,25 +33,19 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Stream<String> getTime() => Stream.periodic(
-//       const Duration(seconds: 1),
-//       (_) => DateTime.now().toIso8601String(),
-//     );
+const url = 'http://bit.ly/48O2oBN';
 
 class MyHomePage extends HookWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // final dateTime = useStream(getTime());
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(() {
-      controller.addListener(() {
-        text.value = controller.text;
-      });
-      return null;
-    });
+    final imageFuture = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
+        .load(url)
+        .then((data) => data.buffer.asUint8List())
+        .then((data) => Image.memory(data)));
+    final snapshot = useFuture(imageFuture);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -48,12 +53,7 @@ class MyHomePage extends HookWidget {
         title: const Text('Home page'),
       ),
       body: Column(
-        children: [
-          TextField(
-            controller: controller,
-          ),
-          Text('You typed ${text.value}')
-        ],
+        children: [snapshot.data].compactMap().toList(),
       ),
     );
   }
