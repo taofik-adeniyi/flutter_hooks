dab2d48bf03e02568facd83d4a2ac0e371e38e3b
Taofik <taofik.dev@gmail.com>
2023-10-01 15:01:04 +0100

    step-6

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,7 @@
 import 'dart:async';
-import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-
-extension CompactMap<T> on Iterable<T?> {
-  Iterable<T> compactMap<E>([
-    E? Function(T?)? transform,
-  ]) =>
-      map(
-        transform ?? (e) => e,
-      ).where((e) => e != null).cast();
-}
 
 void main() {
   runApp(const MyApp());
@@ -37,82 +26,38 @@ class MyApp extends StatelessWidget {
 }
 
 const url = 'https://bit.ly/46oBQFe';
-const imageHeight = 300.0;
-
-extension Normalize on num {
-  num normalized(
-    num selfRangMin,
-    num selfRangeMax, [
-    num normalizedRangeMin = 0.0,
-    num normalizedRangeMax = 1.0,
-  ]) =>
-      (normalizedRangeMax - normalizedRangeMin) *
-          ((this - selfRangMin) / (selfRangeMax - selfRangMin)) +
-      normalizedRangeMin;
-}
 
 class MyHomePage extends HookWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final opacity = useAnimationController(
-      duration: const Duration(seconds: 1),
-      initialValue: 1.0,
-      lowerBound: 0.0,
-      upperBound: 1.0,
-    );
-    final size = useAnimationController(
-      duration: const Duration(seconds: 1),
-      initialValue: 1.0,
-      lowerBound: 0.0,
-      upperBound: 1.0,
-    );
-
-    final controller = useScrollController();
-    useEffect(() {
-      controller.addListener(() {
-        final newOpacity = max(imageHeight - controller.offset, 0.0);
-        final normalized = newOpacity.normalized(0.0, imageHeight).toDouble();
-        opacity.value = normalized;
-        size.value = normalized;
-      });
-      return null;
-    }, [controller]);
+    late final StreamController<double> controller;
+    controller = useStreamController<double>(onListen: () {
+      controller.sink.add(0.0);
+    });
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // title: Text(dateTime.data ?? 'Home page'),
         title: const Text('Home page'),
       ),
-      body: Column(
-        children: [
-          SizeTransition(
-            sizeFactor: size,
-            axis: Axis.vertical,
-            axisAlignment: -1.0,
-            child: FadeTransition(
-              opacity: opacity,
-              child: Image.network(
-                url,
-                height: imageHeight,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              controller: controller,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('Person ${index + 1}'),
-                );
-              },
-              itemCount: 100,
-            ),
-          )
-        ],
-      ),
+      body: StreamBuilder<double>(
+          stream: controller.stream,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const CircularProgressIndicator();
+            } else {
+              final rotation = snapshot.data ?? 0.0;
+              return GestureDetector(
+                onTap: () {
+                  controller.sink.add(rotation + 10.0);
+                },
+                child: RotationTransition(
+                    turns: AlwaysStoppedAnimation(rotation / 360.0),
+                    child: Center(child: Image.network(url))),
+              );
+            }
+          }),
     );
   }
 }
